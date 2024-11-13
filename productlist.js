c3485863f33e51cf0443912f5555f8d7781229e4
Slowlylee1031 <slowly.s.lee@gmail.com>
2024-11-13 12:13:23 +0800

    Add files via upload

---
+++ productlist.js
@@ -0,0 +1,36 @@
+let productList = [
+        {
+            product: [
+                {
+                    id: "PS0001",
+                    name: "One-shoulder bubble-hem dress",
+                    color: [
+                        // first product
+                        {
+                            {
+                                size : 32,
+                                Qty : 10
+                            },
+                            {
+                                size : 34,
+                                Qty : 10
+                            }
+                        },
+                        // secound Product
+                        {
+                            {
+                                size : 32,
+                                Qty : 10
+                            },
+                            {
+                                size : 34,
+                                Qty : 10
+                            }
+                        }
+
+                    ]
+                }
+            ]
+        }
+    ]
+
